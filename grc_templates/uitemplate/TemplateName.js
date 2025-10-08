42ab32504467373d966b60d83342c960a9df0fbc
champix56 <desorbaix@free.fr>
2025-10-08 14:03:07 +0100

    generate-react-cli plus template basic ui

---
+++ grc_templates/uitemplate/TemplateName.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TemplateName.css'
+const TemplateName = (props) => {
+  return (
+    <div className="TemplateName" data-testid="TemplateName">
+      TemplateName Component
+    </div>
+  )
+};
+export default TemplateName;
