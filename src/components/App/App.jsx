6d4c9b3d40809ddb0f281f9f71bf6b2453b57c61
champix56 <desorbaix@free.fr>
2025-10-08 16:43:40 +0100

    button avec children and default values

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,30 @@
 import "./App.css";
 import Button from "../ui/Button/Button.jsx";
+
+import "./App.css";
 const App = () => {
   return (
     <>
       <div>DEMAT Breizh</div>
       <Button
-        value="Benjammin"
-        nb={1285}
-        arr={[1, 2]}
-        bool={true}
-        func={() => {}}
-        obj={{ a: "c" }}
-      />
+        type="button"
+        style={{
+          textDecoration: "underline",
+          fontStyle: "italic",
+          backgroundColor: "grey",
+        }}
+      >
+        Benjammin
+      </Button>
+      <Button>
+        <div>coucou</div>
+      </Button>
+      <br />
+      <Button>
+        <div>div1</div>
+        <div>div2</div>
+      </Button>
+      <Button  />
     </>
   );
 };
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import "./Button.css";
-const Button = (props) => {
-  console.trace(props);
+import cmpstyle from "./Button.module.css";
+const Button = ({ style, bgcolor, children, type = "button" }) => {
+  console.log(children);
+ 
   return (
-    <button className="Button" data-testid="Button">
-      {props.value}
+    <button
+      type={type}
+      className={cmpstyle.Button}
+      style={{ ...style, backgroundColor: bgcolor }}
+      data-testid="Button"
+    >
+      {children}
     </button>
   );
 };
+
 export default Button;
