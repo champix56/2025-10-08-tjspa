7c91251af5b259321a509c0e8eb8305a6e88d25a
champix56 <desorbaix@free.fr>
2025-10-09 22:46:01 +0200

    gestion des liens

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import style from './Navbar.module.css'
+import React from "react";
+import style from "./Navbar.module.css";
+import { Link } from "react-router";
 const Navbar = (props) => {
   return (
     <div className={style.Navbar} data-testid="Navbar">
-      Navbar Component
+      <Link to={"/"}>Home</Link>&nbsp;|&nbsp;
+      <Link to={"/Editor"}>Nouveau...</Link>&nbsp;|&nbsp;
+      <Link to={"/Thumbnail"}>Thumbnail</Link>&nbsp;|&nbsp;
+      <Link to={"/Editor/2"}>Edit meme ID:2</Link>
     </div>
-  )
+  );
 };
 export default Navbar;
