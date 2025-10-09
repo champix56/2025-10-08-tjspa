7a577b5c0737446d5b58d67f49d24f40a943ccc4
champix56 <desorbaix@free.fr>
2025-10-09 21:26:54 +0200

    connexion d'un composant sans implem react-redux  par HOC avec injection dans les props

--- src/components/functionnal/MemeForm/MemeForm.stored.jsx
+++ src/components/functionnal/MemeForm/MemeForm.stored.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import UnconnectedMemeForm from './MemeForm'
-const MemeForm = (props) => {
-  return (
-    <UnconnectedMemeForm   {...props}/>
-  )
-}
+import React, { useState } from "react";
+import UnconnectedMemeForm from "./MemeForm";
 
-export default MemeForm
+import store from "../../../store/store";
+import { update } from "../../../store/current";
+const MemeForm = (props) => {
+  const [current, setcurrent] = useState(store.getState().current.meme);
+  const [images, setimages] = useState(store.getState().ressources.images);
+
+  return (
+    <UnconnectedMemeForm
+      {...props}
+      images={images}
+      meme={current}
+      onMemeChange={(newMeme) => store.dispatch(update(newMeme))}
+    />
+  );
+};
+
+export default MemeForm;
