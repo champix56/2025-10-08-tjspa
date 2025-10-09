a6c8855cf1766c7ca4a06bc357a1b941087d40ff
champix56 <desorbaix@free.fr>
2025-10-09 22:53:13 +0200

    bootstrap navbar

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import style from "./Navbar.module.css";
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../../../../node_modules/bootstrap/dist/js/bootstrap.js'
+import { Container, Nav, Navbar as NavB } from "react-bootstrap";
 import { Link } from "react-router";
+
 const Navbar = (props) => {
   return (
-    <div className={style.Navbar} data-testid="Navbar">
-      <Link to={"/"}>Home</Link>&nbsp;|&nbsp;
-      <Link to={"/Editor"}>Nouveau...</Link>&nbsp;|&nbsp;
-      <Link to={"/Thumbnail"}>Thumbnail</Link>&nbsp;|&nbsp;
-      <Link to={"/Editor/2"}>Edit meme ID:2</Link>
+    <div className={style.Navbar} data-testid="Navbar"> 
+   <NavB bg="primary" data-bs-theme="dark">
+        <Container>
+          <Link to="/" className="navbar-brand">Navbar</Link>
+          <Nav className="me-auto">
+            <Link className="nav-link" to={"/"}>Home</Link>
+            <Link className="nav-link" to="/Editor">Nouveau...</Link>
+            <Link className="nav-link" to="/Thumbnail">Thumbnail</Link>
+            <Link className="nav-link" to="/Editor/2">Edit meme ID:2</Link>
+          </Nav>
+        </Container>
+      </NavB>
+
     </div>
   );
 };
