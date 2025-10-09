5d2ec3492f9ea6e3c1c6360700b4db226f6da3cc
champix56 <desorbaix@free.fr>
2025-10-09 21:18:04 +0200

    traitement des results de l'execution de l'assync thunk

--- src/store/ressources.ts
+++ src/store/ressources.ts
@@ -1,31 +1,39 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
-import {REST_ADR, REST_RESSOURCES} from '../config/constantes.js'
-interface IRessourcesState{
-    memes:Array<MemeInterface>
-    images:Array<ImageInterface>
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+import { REST_ADR, REST_RESSOURCES } from "../config/constantes.js";
+interface IRessourcesState {
+  memes: Array<MemeInterface>;
+  images: Array<ImageInterface>;
 }
-const initialState:IRessourcesState = {
-    memes:[],
-    images:[],
-}
+const initialState: IRessourcesState = {
+  memes: [],
+  images: [],
+};
 
 const ressources = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(loadRessources.fulfilled, (s, a) => {
+        console.log(a.type);
+      s.images = a.payload.images;
+      s.memes = a.payload.memes;
+    });
+  },
 });
 
-//appels async de chargement 
-export const loadRessources=createAsyncThunk('ressources/load', async()=>{
-    const primages=fetch(`${REST_ADR}${REST_RESSOURCES.images}`)
-    const prmemes=fetch(`${REST_ADR}${REST_RESSOURCES.memes}`)
-    const synchronizedPromise=await Promise.all([primages,prmemes]);
-    return {images:await synchronizedPromise[0].json(),memes:await synchronizedPromise[1].json()}
-
-})
-
+//appels async de chargement
+export const loadRessources = createAsyncThunk("ressources/load", async () => {
+  const primages = fetch(`${REST_ADR}${REST_RESSOURCES.images}`);
+  const prmemes = fetch(`${REST_ADR}${REST_RESSOURCES.memes}`);
+  const synchronizedPromise = await Promise.all([primages, prmemes]);
+  return {
+    images: await synchronizedPromise[0].json(),
+    memes: await synchronizedPromise[1].json(),
+  };
+});
 
 //export const {} = ressources.actions
-const ressourcesReducer=ressources.reducer;
+const ressourcesReducer = ressources.reducer;
 export default ressourcesReducer;
