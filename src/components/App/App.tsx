34341164c203ea8cb33146ccdc97634efcfa9bd8
champix56 <desorbaix@free.fr>
2025-10-09 22:01:40 +0200

    mie en oeuvre HOC avec react-redux

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,52 +1,20 @@
 import "./App.css";
-import "./App.css";
 import FlexHGrow3 from "../layout/FlexHGrow3/FlexHGrow3";
 import FlexVGrow1 from "../layout/FlexVGrow1/FlexVGrow1";
 import Header from "../ui/Header/Header.jsx";
 import Navbar from "../ui/Navbar/Navbar";
-import MemeForm from "../functionnal/MemeForm/MemeForm";
-import {
-  emptyMeme,
-  MemeSVGViewer,
-  type ImageInterface,
-  type MemeInterface,
-} from "orsys-tjs-meme";
+import MemeSVGViewer from "../ui/MemeSVGViewer/MemeSVGViewer.stored.jsx";
+import MemeForm from "../functionnal/MemeForm/MemeForm.stored";
 import Footer from "../ui/Footer/Footer";
-import { REST_ADR, REST_RESSOURCES } from "../../config/constantes.js";
-import { useEffect, useState } from "react";
-import store from '../../store/store.ts'
-// import {memes as rest_memes,images as rest_imgs} from '../../../db/db.json'
-const App = () => {
-  const [currentMeme, setCurrentMeme] = useState<MemeInterface>(emptyMeme);
-  const [images, setImages] = useState<Array<ImageInterface>>([]);
-  useEffect(() => {
-    //mise a jour de l'etat actuel avec etat du store
-    setCurrentMeme(store.getState().current.meme);
-    setImages(store.getState().ressources.images);
-    //mise a jour pour les changements ulterieurs
-    store.subscribe(()=>{
-      setCurrentMeme(store.getState().current.meme);
-      setImages(store.getState().ressources.images);
-    })
 
-  }, []);
+const App = () => {
   return (
     <FlexHGrow3>
       <Header />
       <Navbar />
       <FlexVGrow1>
-        <MemeSVGViewer
-          basePath=""
-          image={images.find((img) => img.id == currentMeme.imageId)}
-          meme={currentMeme}
-        />
-        <MemeForm
-          images={images}
-          meme={currentMeme}
-          onMemeChange={(newMeme: MemeInterface) => {
-            setCurrentMeme(newMeme);
-          }}
-        />
+        <MemeSVGViewer basePath="" />
+        <MemeForm />
       </FlexVGrow1>
       <Footer />
     </FlexHGrow3>
