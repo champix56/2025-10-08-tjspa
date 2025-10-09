f315bc02b5714f6e847a43d66bd2f08b200122dc
champix56 <desorbaix@free.fr>
2025-10-09 21:14:14 +0200

    async fetch on reducer

--- src/store/ressources.ts
+++ src/store/ressources.ts
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
-
+import {REST_ADR, REST_RESSOURCES} from '../config/constantes.js'
 interface IRessourcesState{
     memes:Array<MemeInterface>
     images:Array<ImageInterface>
@@ -15,6 +15,16 @@ const ressources = createSlice({
   initialState,
   reducers: {}
 });
+
+//appels async de chargement 
+export const loadRessources=createAsyncThunk('ressources/load', async()=>{
+    const primages=fetch(`${REST_ADR}${REST_RESSOURCES.images}`)
+    const prmemes=fetch(`${REST_ADR}${REST_RESSOURCES.memes}`)
+    const synchronizedPromise=await Promise.all([primages,prmemes]);
+    return {images:await synchronizedPromise[0].json(),memes:await synchronizedPromise[1].json()}
+
+})
+
 
 //export const {} = ressources.actions
 const ressourcesReducer=ressources.reducer;
