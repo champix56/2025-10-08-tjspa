b215936c7c54bff842a1a2e142a3e7cf96dfaf18
champix56 <desorbaix@free.fr>
2025-10-10 16:01:38 +0100

    context de router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App/App'
-import { Provider } from 'react-redux'
-import store from './store/store'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import {BrowserRouter as  Router } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
