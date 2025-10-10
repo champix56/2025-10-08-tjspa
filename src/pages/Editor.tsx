7267ed1b37e8947c3765d72570cb99d06c1b7921
champix56 <desorbaix@free.fr>
2025-10-10 16:01:25 +0100

    pages pour routes

---
+++ src/pages/Editor.tsx
@@ -0,0 +1,13 @@
+import MemeSvgViewer from "../components/ui/MemeSVGViewer/MemeSVGViewer.stored";
+import MemeForm from "../components/functionnal/MemeForm/MemeForm.stored";
+
+const Editor = () => {
+  return (
+    <>
+      <MemeSvgViewer basePath="" />
+      <MemeForm />
+    </>
+  );
+};
+
+export default Editor;
